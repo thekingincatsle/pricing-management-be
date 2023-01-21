5bdb462da5e4ffde2775083ed31624f93e98b2f6
hieu <mhieu.vn@gmail.com>
2023-01-21 23:25:43 +0700

    fix

--- src/main/java/com/example/pricemanagement/repository/FormDangKyRepository.java
+++ src/main/java/com/example/pricemanagement/repository/FormDangKyRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 @Repository
 public class FormDangKyRepository {
-    private FormDangKyBaseRepository formDangKyBaseRepository ;
+    private final FormDangKyBaseRepository formDangKyBaseRepository ;
 
     public FormDangKyRepository(FormDangKyBaseRepository formDangKyBaseRepository) {
         this.formDangKyBaseRepository = formDangKyBaseRepository;
